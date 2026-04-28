63351feb258ff6e513a4e7135e7a7ac035425066
lunakittywolf <276470916+lunakittywolf@users.noreply.github.com>
2026-04-27 23:37:23 -0500

    Mobile Formatting Update

--- scripts/journal.js
+++ scripts/journal.js
@@ -16,3 +16,14 @@ $(".picture").on("mouseleave",function(){
     $(this).css("transform",$(this).css("transform").replace("scale(2)","") + "scale(.5)")
     $(this).css("z-index",0);
 })
+
+const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+
+if (isMobile) {
+    $("img").css("width","-webkit-fill-available")
+    $(".picture").css("width","-webkit-fill-available")
+    $(".picture-box").css("width","-webkit-fill-available")
+    $("#entry").css("width","90%");
+    $("body").css("width","100%");
+    $("body").css("height","auto");
+}
